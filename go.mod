3258958186358973c5d8bbe14276050f7f6920df
liuhaibo.2020 <liuhaibo.2020@bytedance.com>
2020-09-01 18:15:35 +0800

    update homepage log
    Change-Id: I750a441ab8b93c4a2128047cacd8507dd5a88bf4

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/KiwanoEngine/KiwanoWeb
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/madhums/go-gin-mgo-demo v0.0.0-20150808235628-3c308faae11f
+	github.com/sirupsen/logrus v1.6.0
+	github.com/spf13/viper v1.7.1
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+)
--- routers/routers.go
+++ routers/routers.go
@@ -1,25 +1,37 @@
 package routers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 
-	"github.com/KiwanoEngine/KiwanoWeb/controllers"
 	"github.com/KiwanoEngine/KiwanoWeb/modules/templates"
+	"github.com/KiwanoEngine/KiwanoWeb/render"
 )
 
 func Setup(engine *gin.Engine) {
 	engine.Delims("{{", "}}")
 	engine.SetFuncMap(templates.FuncMap)
-	engine.LoadHTMLFiles(
-		"./views/index.html",
-		"./views/go-index.html",
-		"./views/go-package.html",
-		"./views/go-demos-package.html",
-	)
-	engine.Static("/static", "./static")
+	engine.Static("/static", "./static_web/static")
 
-	engine.GET("/", controllers.Home)
-	engine.GET("/go", controllers.GoHome)
-	engine.GET("/kiwano", controllers.GoPackageService)
-	engine.GET("/demos", controllers.GoDemosPackageService)
+	htmlRender := render.NewHTMLRender()
+	htmlRender.TemplatesDir = "static_web/"
+	htmlRender.Ext = ".html"
+	engine.HTMLRender = htmlRender.Create()
+
+	// Homepage
+	engine.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index", nil)
+	})
+
+	// Kiwano for golang
+	engine.GET("/go", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "go/index", nil)
+	})
+	engine.GET("/kiwano", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "kiwano/index", nil)
+	})
+	engine.GET("/demos", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "demos/index", nil)
+	})
 }
